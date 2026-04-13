c6d6f477369f0b49bd5c95c3390f5a5557c68b41
piwer <pipodany5@gmail.com>
2026-04-13 18:40:53 +0300

    environment

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,4 +1,4 @@
 // lib/environment.dart
 class Environment {
-  static const String apiUrl = 'https://loopbackend-4rpp.onrender.com/api/'; //https://loopbackend-4rpp.onrender.com/api/
+  static const String apiUrl = 'https://looprealestate.me/api/'; //https://loopbackend-4rpp.onrender.com/api/
 }
